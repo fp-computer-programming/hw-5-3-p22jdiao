72fa7ced6f5bb9e651304f47d92d917ee83bde7d
p22jdiao <p22jdiao@fairfieldprep.org>
2021-11-02 10:04:42 -0400

    5-3-4

--- hw5-3.py
+++ hw5-3.py
@@ -1,3 +1,25 @@
 import calendar
 
-help(calendar)
+# Question 1
+
+calendar.TextCalendar().pryear(2020)
+
+# Question 2
+
+calendar.setfirstweekday(calendar.SUNDAY)
+
+print(calendar.calendar(2020))
+
+# Question 3
+
+calendar.prmonth(2003,1)
+
+# Question 4
+
+calendar.LocaleTextCalendar(6, "SPAINISH").pryear(2020)
+
+# Question 5
+
+print(calendar.isleap(2003))
+
+# It determines if the given year is a leap year of not. It returns True of False as a result, it is a boolean value.
